80e7c8608a1ad443025f4b8ec5fcbd80c389be08
Madaicol <artmadai@hotmail.com>
2016-08-15 16:07:22 -0500

    Variables termiandas

--- IAgentes/assets/Angular/Controllers/variables.js
+++ IAgentes/assets/Angular/Controllers/variables.js
@@ -1,3 +1,14 @@
-applicacion.controller('variablescontroller',['$scope','toastr',function($scope,toastr){
-    toastr.info('Info','Entraste a la vista 1');
+applicacion.controller('variablescontroller', ['$scope', 'toastr', '$http', function($scope, toastr, $http) {
+  toastr.info('Info', 'Entraste a la vista 1');
+  // $scope.Clima = "Soleado";
+  $scope.variables = {
+    agClima: null,
+    agTrafico: null,
+    agDiaNoche: null
+  }
+
+  $scope.agregarNuevaVariable = function() {
+
+  }
+
 }]);
---
+++ IAgentes/assets/Angular/Factory/factory.js
@@ -0,0 +1,53 @@
+
+
+applicacion.factory('VariablesFactory',['$resource',function($resource){
+
+    var factory = $resource(
+        'http://localhost:1337/Entrenador/:idEntrenador',
+    {
+        idEntrenador:'@idEntrenador'
+    },
+         {
+          actualizar:
+            {
+                method:'PUT',
+                params:{
+                    idEntrenador:'@idEntrenador'
+                }
+
+            }
+         });
+
+    return factory;
+
+}]);
+
+
+
+
+applicacion.factory('MascotaFactory',['$resource',function($resource){
+    var factory = $resource(
+        'http://localhost:1337/Mascota/:idMascota',
+    {
+        idMascota:'@idMascota'
+    },
+         {
+          actualizar:
+            {
+                method:'PUT',
+                params:{
+                    idMascota:'@idMascota'
+                }
+            },
+         busquedaPorIdEntrenador:
+            {
+                url:'http://localhost:1337/Mascota?idEntrenador=:idEntrenador',
+                method:'GET',
+                params:{
+                    idEntrenador:'@idEntrenador'
+                },
+                isArray:true
+            }
+         });
+    return factory;
+}]);
